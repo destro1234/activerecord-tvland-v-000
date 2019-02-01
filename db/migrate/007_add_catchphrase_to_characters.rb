840262f95bfb3cb1b2046d617645fb411c343dae
Elias Ventura <eliasventura23@gmail.com>
2019-02-01 18:46:38 +0000

    Automatically backed up by Learn

--- db/migrate/007_add_catchphrase_to_characters.rb
+++ db/migrate/007_add_catchphrase_to_characters.rb
@@ -1,5 +1,5 @@
 class AddCatchphraseToCharacters < ActiveRecord::Migration[4.2]
   def change
-  add_column :character, :catchprase, :string
-end 
+    add_column :character, :catchprase, :string
+  end
 end
